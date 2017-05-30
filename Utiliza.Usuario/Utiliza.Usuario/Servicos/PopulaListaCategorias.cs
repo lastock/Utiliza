1e49d54e556232a6845af81e2c13f29e62db1909
Luis Alfredo Stockler Filho <lastock@outlook.com.br>
2017-05-30 13:32:08 -0300

    30/05/17 13:31

--- Utiliza.Usuario/Utiliza.Usuario/Servicos/PopulaListaCategorias.cs
+++ Utiliza.Usuario/Utiliza.Usuario/Servicos/PopulaListaCategorias.cs
@@ -12,22 +12,25 @@ namespace Utiliza.Usuario.Servicos
         public List<Categoria> Popula()
         {
             var _categorias = new List<Categoria>();
-            _categorias.Add(new Categoria { IdCategoria = 1, NomeCategoria = "Alimentação", Ordem = 1, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 2, NomeCategoria = "Moda", Ordem = 2, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 3, NomeCategoria = "Serviços", Ordem = 3, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 4, NomeCategoria = "Saúde", Ordem = 4, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 5, NomeCategoria = "Beleza", Ordem = 5, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 6, NomeCategoria = "Produtos", Ordem = 6, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 7, NomeCategoria = "Casa", Ordem = 7, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 8, NomeCategoria = "Festas", Ordem = 8, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 9, NomeCategoria = "Lazer", Ordem = 9, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 10, NomeCategoria = "Automóveis", Ordem = 10, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 11, NomeCategoria = "Turismo", Ordem = 11, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 12, NomeCategoria = "Animais", Ordem = 12, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 13, NomeCategoria = "Taxi", Ordem = 13, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 14, NomeCategoria = "Onibus", Ordem = 14, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 15, NomeCategoria = "Igrejas", Ordem = 15, UrlLogo = "teste.png" });
-            _categorias.Add(new Categoria { IdCategoria = 16, NomeCategoria = "Serviços", Ordem = 16, UrlLogo = "teste.png" });
+            _categorias.Add(new Categoria { IdCategoria = 1, NomeCategoria = "Alimentação", Ordem = 1, UrlLogo = "alimentacao.png" });
+            _categorias.Add(new Categoria { IdCategoria = 2, NomeCategoria = "Moda", Ordem = 2, UrlLogo = "moda.png" });
+            _categorias.Add(new Categoria { IdCategoria = 3, NomeCategoria = "Serviços", Ordem = 3, UrlLogo = "servicos.png" });
+            _categorias.Add(new Categoria { IdCategoria = 4, NomeCategoria = "Saúde", Ordem = 4, UrlLogo = "saude.png" });
+            _categorias.Add(new Categoria { IdCategoria = 5, NomeCategoria = "Beleza e Estética", Ordem = 5, UrlLogo = "belezaestetica.png" });
+            _categorias.Add(new Categoria { IdCategoria = 6, NomeCategoria = "Produtos", Ordem = 6, UrlLogo = "produtos.png" });
+            _categorias.Add(new Categoria { IdCategoria = 7, NomeCategoria = "Casa e Decoração", Ordem = 7, UrlLogo = "casadecoracao.png" });
+            _categorias.Add(new Categoria { IdCategoria = 8, NomeCategoria = "Festas e Eventos", Ordem = 8, UrlLogo = "festaseventos.png" });
+            _categorias.Add(new Categoria { IdCategoria = 9, NomeCategoria = "Esportes e Lazer", Ordem = 9, UrlLogo = "esporteslazer.png" });
+            _categorias.Add(new Categoria { IdCategoria = 10, NomeCategoria = "Automóveis", Ordem = 10, UrlLogo = "automoveis.png" });
+            _categorias.Add(new Categoria { IdCategoria = 11, NomeCategoria = "Turismo", Ordem = 11, UrlLogo = "turismo.png" });
+            _categorias.Add(new Categoria { IdCategoria = 12, NomeCategoria = "Animais", Ordem = 12, UrlLogo = "animais.png" });
+            _categorias.Add(new Categoria { IdCategoria = 13, NomeCategoria = "Taxi", Ordem = 13, UrlLogo = "taxi.png" });
+            _categorias.Add(new Categoria { IdCategoria = 14, NomeCategoria = "Calçados", Ordem = 14, UrlLogo = "calcados.png" });
+            _categorias.Add(new Categoria { IdCategoria = 15, NomeCategoria = "Igrejas", Ordem = 15, UrlLogo = "igrejas.png" });
+            _categorias.Add(new Categoria { IdCategoria = 16, NomeCategoria = "Farmácias", Ordem = 16, UrlLogo = "farmacia.png" });
+            _categorias.Add(new Categoria { IdCategoria = 17, NomeCategoria = "Reparos", Ordem = 16, UrlLogo = "reparos.png" });
+            _categorias.Add(new Categoria { IdCategoria = 18, NomeCategoria = "Saúde e Fitness", Ordem = 16, UrlLogo = "saudefitness.png" });
+            _categorias.Add(new Categoria { IdCategoria = 19, NomeCategoria = "Serviços Públicos", Ordem = 16, UrlLogo = "publicos.png" });
             return _categorias;
         }
 
